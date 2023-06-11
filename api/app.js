aeac42d80687cded0ccebf42442cf73f495b8aee
Badal kumar Prusty <99793166+Mr-BadalKumar@users.noreply.github.com>
2023-06-11 18:44:13 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -8,103 +8,21 @@ const bodyParser = require('body-parser');
 // Load in the mongoose models
 const { List, Task, User } = require('./db/models');
 
-const jwt = require('jsonwebtoken');
-
-
 /* MIDDLEWARE  */
 
 // Load middleware
 app.use(bodyParser.json());
 
-
 // CORS HEADERS MIDDLEWARE
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
-
-    res.header(
-        'Access-Control-Expose-Headers',
-        'x-access-token, x-refresh-token'
-    );
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     next();
 });
 
-
-// check whether the request has a valid JWT access token
-let authenticate = (req, res, next) => {
-    let token = req.header('x-access-token');
-
-    // verify the JWT
-    jwt.verify(token, User.getJWTSecret(), (err, decoded) => {
-        if (err) {
-            // there was an error
-            // jwt is invalid - * DO NOT AUTHENTICATE *
-            res.status(401).send(err);
-        } else {
-            // jwt is valid
-            req.user_id = decoded._id;
-            next();
-        }
-    });
-}
-
-// Verify Refresh Token Middleware (which will be verifying the session)
-let verifySession = (req, res, next) => {
-    // grab the refresh token from the request header
-    let refreshToken = req.header('x-refresh-token');
-
-    // grab the _id from the request header
-    let _id = req.header('_id');
-
-    User.findByIdAndToken(_id, refreshToken).then((user) => {
-        if (!user) {
-            // user couldn't be found
-            return Promise.reject({
-                'error': 'User not found. Make sure that the refresh token and user id are correct'
-            });
-        }
-
-
-        // if the code reaches here - the user was found
-        // therefore the refresh token exists in the database - but we still have to check if it has expired or not
-
-        req.user_id = user._id;
-        req.userObject = user;
-        req.refreshToken = refreshToken;
-
-        let isSessionValid = false;
-
-        user.sessions.forEach((session) => {
-            if (session.token === refreshToken) {
-                // check if the session has expired
-                if (User.hasRefreshTokenExpired(session.expiresAt) === false) {
-                    // refresh token has not expired
-                    isSessionValid = true;
-                }
-            }
-        });
-
-        if (isSessionValid) {
-            // the session is VALID - call next() to continue with processing this web request
-            next();
-        } else {
-            // the session is not valid
-            return Promise.reject({
-                'error': 'Refresh token has expired or the session is invalid'
-            })
-        }
-
-    }).catch((e) => {
-        res.status(401).send(e);
-    })
-}
-
 /* END MIDDLEWARE  */
-
-
-
 
 /* ROUTE HANDLERS */
 
@@ -114,46 +32,43 @@ let verifySession = (req, res, next) => {
  * GET /lists
  * Purpose: Get all lists
  */
-app.get('/lists', authenticate, (req, res) => {
-    // We want to return an array of all the lists that belong to the authenticated user 
-    List.find({
-        _userId: req.user_id
-    }).then((lists) => {
+app.get('/lists', (req, res) => {
+    // Return an array of all the lists
+    List.find().then((lists) => {
         res.send(lists);
     }).catch((e) => {
         res.send(e);
     });
-})
+});
 
 /**
  * POST /lists
  * Purpose: Create a list
  */
-app.post('/lists', authenticate, (req, res) => {
-    // We want to create a new list and return the new list document back to the user (which includes the id)
+app.post('/lists', (req, res) => {
+    // Create a new list and return the new list document back to the user
     // The list information (fields) will be passed in via the JSON request body
     let title = req.body.title;
 
     let newList = new List({
-        title,
-        _userId: req.user_id
+        title
     });
     newList.save().then((listDoc) => {
-        // the full list document is returned (incl. id)
+        // The full list document is returned (including the id)
         res.send(listDoc);
-    })
+    });
 });
 
 /**
  * PATCH /lists/:id
  * Purpose: Update a specified list
  */
-app.patch('/lists/:id', authenticate, (req, res) => {
-    // We want to update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
-    List.findOneAndUpdate({ _id: req.params.id, _userId: req.user_id }, {
+app.patch('/lists/:id', (req, res) => {
+    // Update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+    List.findOneAndUpdate({ _id: req.params.id }, {
         $set: req.body
     }).then(() => {
-        res.send({ 'message': 'updated successfully'});
+        res.send({ 'message': 'Updated successfully' });
     });
 });
 
@@ -161,100 +76,81 @@ app.patch('/lists/:id', authenticate, (req, res) => {
  * DELETE /lists/:id
  * Purpose: Delete a list
  */
-app.delete('/lists/:id', authenticate, (req, res) => {
-    // We want to delete the specified list (document with id in the URL)
+app.delete('/lists/:id', (req, res) => {
+    // Delete the specified list (document with id in the URL)
     List.findOneAndRemove({
-        _id: req.params.id,
-        _userId: req.user_id
+        _id: req.params.id
     }).then((removedListDoc) => {
         res.send(removedListDoc);
 
-        // delete all the tasks that are in the deleted list
+        // Delete all the tasks that are in the deleted list
         deleteTasksFromList(removedListDoc._id);
-    })
+    });
 });
-
 
 /**
  * GET /lists/:listId/tasks
  * Purpose: Get all tasks in a specific list
  */
-app.get('/lists/:listId/tasks', authenticate, (req, res) => {
-    // We want to return all tasks that belong to a specific list (specified by listId)
+app.get('/lists/:listId/tasks', (req, res) => {
+    // Return all tasks that belong to a specific list (specified by listId)
     Task.find({
         _listId: req.params.listId
     }).then((tasks) => {
         res.send(tasks);
-    })
+    });
 });
-
 
 /**
  * POST /lists/:listId/tasks
  * Purpose: Create a new task in a specific list
  */
-app.post('/lists/:listId/tasks', authenticate, (req, res) => {
-    // We want to create a new task in a list specified by listId
+app.post('/lists/:listId/tasks', (req, res) => {
+    // Create a new task in a list specified by listId
 
     List.findOne({
-        _id: req.params.listId,
-        _userId: req.user_id
+        _id: req.params.listId
     }).then((list) => {
         if (list) {
-            // list object with the specified conditions was found
-            // therefore the currently authenticated user can create new tasks
-            return true;
-        }
-
-        // else - the list object is undefined
-        return false;
-    }).then((canCreateTask) => {
-        if (canCreateTask) {
+            // List object with the specified conditions was found
+            // Create a new task
             let newTask = new Task({
                 title: req.body.title,
-                _listId: req.params.listId
+                _listId: list._id
             });
             newTask.save().then((newTaskDoc) => {
                 res.send(newTaskDoc);
             })
         } else {
+            // List object is undefined
             res.sendStatus(404);
         }
     })
-})
+});
 
 /**
  * PATCH /lists/:listId/tasks/:taskId
  * Purpose: Update an existing task
  */
-app.patch('/lists/:listId/tasks/:taskId', authenticate, (req, res) => {
-    // We want to update an existing task (specified by taskId)
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    // Update an existing task (specified by taskId)
 
     List.findOne({
-        _id: req.params.listId,
-        _userId: req.user_id
+        _id: req.params.listId
     }).then((list) => {
         if (list) {
-            // list object with the specified conditions was found
-            // therefore the currently authenticated user can make updates to tasks within this list
-            return true;
-        }
-
-        // else - the list object is undefined
-        return false;
-    }).then((canUpdateTasks) => {
-        if (canUpdateTasks) {
-            // the currently authenticated user can update tasks
+            // List object with the specified conditions was found
+            // Find and update the task
             Task.findOneAndUpdate({
                 _id: req.params.taskId,
-                _listId: req.params.listId
+                _listId: list._id
             }, {
-                    $set: req.body
-                }
-            ).then(() => {
-                res.send({ message: 'Updated successfully.' })
-            })
+                $set: req.body
+            }).then(() => {
+                res.send({ 'message': 'Updated successfully' });
+            });
         } else {
+            // List object is undefined
             res.sendStatus(404);
         }
     })
@@ -264,36 +160,27 @@ app.patch('/lists/:listId/tasks/:taskId', authenticate, (req, res) => {
  * DELETE /lists/:listId/tasks/:taskId
  * Purpose: Delete a task
  */
-app.delete('/lists/:listId/tasks/:taskId', authenticate, (req, res) => {
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    // Delete an existing task (specified by taskId)
 
     List.findOne({
-        _id: req.params.listId,
-        _userId: req.user_id
+        _id: req.params.listId
     }).then((list) => {
         if (list) {
-            // list object with the specified conditions was found
-            // therefore the currently authenticated user can make updates to tasks within this list
-            return true;
-        }
-
-        // else - the list object is undefined
-        return false;
-    }).then((canDeleteTasks) => {
-        
-        if (canDeleteTasks) {
+            // List object with the specified conditions was found
+            // Find and remove the task
             Task.findOneAndRemove({
                 _id: req.params.taskId,
-                _listId: req.params.listId
+                _listId: list._id
             }).then((removedTaskDoc) => {
                 res.send(removedTaskDoc);
-            })
+            });
         } else {
+            // List object is undefined
             res.sendStatus(404);
         }
-    });
+    })
 });
-
-
 
 /* USER ROUTES */
 
@@ -328,7 +215,6 @@ app.post('/users', (req, res) => {
     })
 })
 
-
 /**
  * POST /users/login
  * Purpose: Login
@@ -358,12 +244,11 @@ app.post('/users/login', (req, res) => {
     });
 })
 
-
 /**
  * GET /users/me/access-token
  * Purpose: generates and returns an access token
  */
-app.get('/users/me/access-token', verifySession, (req, res) => {
+app.get('/users/me/access-token', (req, res) => {
     // we know that the user/caller is authenticated and we have the user_id and user object available to us
     req.userObject.generateAccessAuthToken().then((accessToken) => {
         res.header('x-access-token', accessToken).send({ accessToken });
@@ -371,8 +256,6 @@ app.get('/users/me/access-token', verifySession, (req, res) => {
         res.status(400).send(e);
     });
 })
-
-
 
 /* HELPER METHODS */
 let deleteTasksFromList = (_listId) => {
@@ -382,9 +265,6 @@ let deleteTasksFromList = (_listId) => {
         console.log("Tasks from " + _listId + " were deleted!");
     })
 }
-
-
-
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
